ffb76ccf5e9e531f077070ad2b3ffb08d317db70
FancyCodeMaster <fancycodemaster@gmail.com>
2023-02-10 16:02:44 +0545

    sites disable done

--- front_end/src/extension/popup-files/popup.js
+++ front_end/src/extension/popup-files/popup.js
@@ -1,8 +1,28 @@
-const focusModeButton = document.querySelector(".focus-mode-button");
 
-focusModeButton.addEventListener("click" , () => {
-    //document.head.innerHTML
+const youtubeUrl = 'www.youtube.com';
+const tiktokUrl = 'www.tiktok.com';
+const twitterUrl = 'www.twitter.com';
+const instagramUrl = 'www.instagram.com';
+// const Url = 'www.youtube.com';
 
-    alert("Focus mode is on. Websites disabled.");
+if(window.location.hostname === youtubeUrl || window.location.hostname === tiktokUrl || window.location.hostname === twitterUrl || window.location.hostname === instagramUrl){
+    document.body.innerHTML = `
+    <div class="disable_container">
+        <h2>Focus Mode is still on. More to work More to achieve</h2>
+    </div>
+    `;
+    document.head.innerHTML = `
+        <style>
+            .disable_container{
+                height : 100vh;
+                width : 100vw;
+                display : flex;
+                justify_content : center;
+                align_items : center;
+            }
+        </style>
+    `;
 
-});
+}
+
+// background : url(`+"./disable-background.jpg"+`);
