868137135b926f1ba2e76af812b87b8c561bc506
Bishal Poudel <bishalpoudel607@gmail.com>
2023-02-11 00:55:18 +0545

    changes: specific path for todo app

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,12 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from todo import views
 
-router = routers.DefaultRouter()
-router.register(r"todos", views.TodoView, "todo")
+# router = routers.DefaultRouter()
+# router.register(r"todos", views.TodoView, "todo")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("todoapi/", include(router.urls))
+    path("todoapi/", include("todo.urls")),
 ]
