de1a4eb9d831a7ebc4d6444ad6998710fe7ee759
Bishal Poudel <bishalpoudel607@gmail.com>
2023-02-10 22:54:24 +0545

    update: bkend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "corsheaders",
     "rest_framework",  # rest framework
-    "todo",  # todo application
+    "todo",  # todo api
 ]
 
 MIDDLEWARE = [
