bf028969eaeb7d1e80aa4bfbef7e3c41118dc06a
Prince Kushwaha <princekushwaha22@yahoo.com>
2023-02-10 21:06:14 +0545

    added heroBanner

---
+++ front_end/src/Components/Todo/Hero/Hero.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./hero.css";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+        <div className="heroWrapper">
+
+                <div className="heroHeading">
+                    <h1>Focus more in distractive Environment</h1>
+                    <p> Student+ is an automated distraction eliminator that enables you to take control over your priorities once again. With Student+, you can finally be free to pursue your goals distraction-free.</p>
+                </div>
+                <div className="heroCharacter">
+                    <img src={require('../../../images/character1.png')} className="character1"/>
+                    <img src={require('../../../images/character2.png')} className="character2"/>
+                </div>
+
+
+           
+        </div>
+        <div className="btn">
+        <button src="#">Focus Now</button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Hero
