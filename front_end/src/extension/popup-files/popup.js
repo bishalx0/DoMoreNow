42f8ed14030886bddc895418e2adb8b286d5abbd
FancyCodeMaster <fancycodemaster@gmail.com>
2023-02-10 15:35:10 +0545

    disable website extension

---
+++ front_end/src/extension/popup-files/popup.js
@@ -0,0 +1,8 @@
+const focusModeButton = document.querySelector(".focus-mode-button");
+
+focusModeButton.addEventListener("click" , () => {
+    //document.head.innerHTML
+
+    alert("Focus mode is on. Websites disabled.");
+
+});
