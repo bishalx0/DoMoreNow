79cb6aa52c928da4fe7e0fea78364fcb8dd2ae06
FancyCodeMaster <fancycodemaster@gmail.com>
2023-02-10 21:33:06 +0545

    todo file for todo creation

--- front_end/src/Components/Todo/Todo.js
+++ front_end/src/Components/Todo/Todo.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react';
+import classes from './Todo.module.css';
 
 const Todo = () => {
+    const [todoTitle,updateTodoTitle] = useState('');
+    const [hr,updateHr] = useState(0);
+    const [min,updateMin] = useState(0);
+
+    useEffect(() => {
+        console.log(todoTitle,hr,min);
+    })
+
+    const sendData = async () => {
+        try{
+            const response = await fetch('http://localhost:8000/',{
+            method : 'POST',
+            body : JSON.stringify({
+                todoTitle : todoTitle,
+                hrs : hr,
+                mins : min
+            }),
+            headers : {
+                "Content-type" : "application/json , charset=UTF-8",
+            },
+            })
+            const data = await response.json();
+        }catch(error){
+            console.log(error);
+        }
+
+    }
+
+    const onSendDataClick = () => {
+        if(todoTitle !== "" && hr !== 0 && min !== 0){
+            sendData();
+        }
+    }
+
   return (
-    <div>
-      todo
+    <div className={classes.todoContainer}>
+        {/* todo title */}
+        <div className={classes.items}>
+            <input type="text" className={classes.todoTitle} onChange={(el) => updateTodoTitle(el.target.value)} />
+            {/* todo hour */}
+            <input type="number" min="0" max="23" className={classes.timeCount} onChange={(el) => updateHr(el.target.value)}  /><p className={classes.timeTitle}>Hrs</p>
+            <input type="number" min="0" max="59" className={classes.timeCount} onChange={(el) => updateMin(el.target.value)}  /><p className={classes.timeTitle}>Mins</p>
+            <button onClick={onSendDataClick}>Send Data</button>
+        </div>
     </div>
   )
 }
