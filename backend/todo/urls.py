152a44e3b2992514d89cc11fb50ba7690f9357c0
Bishal Poudel <bishalpoudel607@gmail.com>
2023-02-11 16:36:04 +0545

    todo with user stats

--- backend/todo/urls.py
+++ backend/todo/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('todos/', views.TodoList.as_view(), name='todo_list'),
     path('todos/<int:pk>/', views.TodoDetail.as_view(), name='todo_detail'),
+    path("stats/", views.get_stats, name="get_stats")
 ]
--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,19 +1,62 @@
+import datetime
+
 from rest_framework import viewsets
 from .serializers import TodoSerializer
 from rest_framework import generics
 from .models import Todo
 from rest_framework.response import Response
 import math
+from rest_framework.decorators import api_view
 
 # Create your views here.
+
+
+@api_view(['GET'])
+def get_stats(request):
+
+    completed_tasks = Todo.objects.filter(completed=True).count()
+    total_added_tasks = Todo.objects.all().count()
+
+    mins = 0
+    hrs = 0
+    for data in Todo.objects.filter(completed=True):
+        hrs += data.hrs
+        mins += data.mins
+
+    # calculate number of streaks
+    queryset = Todo.objects.filter(completed=True).order_by("-created_at")
+    _date = datetime.date.today()
+    streak = 0
+    for row in queryset.iterator():
+        date = row.created_at.date()
+        streak += 1
+        delta = _date - date
+        if delta.days > 1:
+            break
+
+    data = {
+        "streak": streak,
+        "completed_task": completed_tasks,
+        "added_task": total_added_tasks,
+        "hours": hrs,
+        "minutes": mins
+    }
+
+    return Response(data)
 
 
 class TodoList(generics.ListCreateAPIView):
     """
     TODO API List view
     """
-    serializer_class = TodoSerializer
     queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+    def get(self, request, *args, **kwargs):
+        todos = self.get_queryset()
+        serializer = TodoSerializer(todos, many=True)
+        # print(serializer.data)
+        return Response(serializer.data)
 
 
 class TodoDetail(generics.RetrieveUpdateDestroyAPIView):
